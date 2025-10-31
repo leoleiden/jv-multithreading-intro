767d40e5028c68eecd5892765afea339ba2eaa64
Leonid Lachmann <148564629+leoleiden@users.noreply.github.com>
2025-10-31 21:54:22 +0000

    Extend MyThread class to print numbers 0 to 20

--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -1,5 +1,10 @@
-package core.basesyntax.thread;
+package core.basesyntax;
 
-public class MyThread {
-    // write your code here
+public class MyThread extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i <= 20; i++) {
+            System.out.println(i);
+        }
+    }
 }
