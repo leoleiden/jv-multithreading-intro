7526087646c09625040d6d4d60c98c0acd460a0a
Leonid Lachmann <148564629+leoleiden@users.noreply.github.com>
2025-10-31 21:55:14 +0000

    Add multithreading example in Main.java

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,19 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        // Створюємо та запускаємо потік, що успадковує Thread
+        MyThread myThread = new MyThread();
+
+        // Створюємо екземпляр Runnable
+        MyRunnable myRunnable = new MyRunnable();
+        // Створюємо потік на основі Runnable
+        Thread daemonThread = new Thread(myRunnable);
+
+        // Позначаємо потік як демон
+        daemonThread.setDaemon(true);
+
+        // Запускаємо обидва потоки
+        myThread.start();
+        daemonThread.start();
     }
 }
